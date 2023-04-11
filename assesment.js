9f557637128fa70ed7b16bcb224347ae77872904
MiriamLongoyapus <longoyapusmiriam@gmail.com>
2023-04-11 09:00:48 +0300

    first commit

---
+++ assesment.js
@@ -0,0 +1,61 @@
+let food="eating"
+let food2=food.reverse()
+console.log(food2)
+
+function dividingArray(arr){
+    let left=0
+    let right=arr.length/2
+    while(left.length && right.length){
+        if(left[0]<=right[0]){
+            
+        }
+    }
+}
+
+function binary(num,target){
+    emptyArray=[];
+    while(left.length[0] && right.length[0]){
+        let middle=Math.floor(left+right/2)
+        if(num[middle]===target){
+            return middle
+        }
+        else if(num[middle]<target){
+            left=middle +1
+        }
+        else(num[middle]>target);{
+            right=middle -1
+        }
+        return null
+
+    }
+    
+    
+}
+let num=[2,8,0,23,5,45,76];
+target=23
+console.log(binary(num,target))
+
+function numbers(){
+    if(numbers%3==0){
+        print("Fizz")
+    }
+    else if(numbers%5==0){
+        print("Buzz")
+    }
+    else(numbers%3==0,numbers%5==0);{
+        print("FizzBuzz")
+    }
+}
+
+let numArray=[12,87,45,75,23,64,73];
+multiplication=1;
+i=1
+if(i=numArray.length){
+    multiplication[i] *=4
+
+}
+console.log(numArray)
+
+let nums = [10,24,45,56,67];
+console.log(nums.sort())
+
